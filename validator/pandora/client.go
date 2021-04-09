63ecd2365dabb7fcbb209b0f381c6bb532e879b8
Atif Anowar <atif@lukso.io>
2021-04-09 12:15:07 +0200

    Converted hex to hash

--- validator/pandora/client.go
+++ validator/pandora/client.go
@@ -96,10 +96,9 @@ func (oc *PandoraClient) SubmitShardBlockHeader(ctx context.Context, blockNonce 
 	sig [96]byte) (bool, error) {
 
 	nonecHex := types.EncodeNonce(blockNonce)
-	headerHashHex := headerHash.Hex()
 
 	var status bool
-	if err := oc.c.CallContext(ctx, &status, "eth_submitWorkBLS", nonecHex, headerHashHex, sig); err != nil {
+	if err := oc.c.CallContext(ctx, &status, "eth_submitWorkBLS", nonecHex, headerHash, sig); err != nil {
 		return false, errors.Wrap(err, "Got error when calls to eth_submitWork api")
 	}
 	return status, nil
